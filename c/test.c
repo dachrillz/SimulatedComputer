a3ea2776c9c80c69565f05e11f181dedd53726c7
dachrillz <olschr2@gmail.com>
2018-01-30 22:30:15 +0100

    fixed test suite

--- c/test.c
+++ c/test.c
@@ -1,19 +1,65 @@
 #include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
 #include "src/myfile.h"
-
+#include <stdio.h>
 
 void test_add(void);
+void test_minus(void);
 
-int main(){
-test_add();
+
+int init_suite(void) { return 0; }
+int clean_suite(void) { return 0; }
+
+int main(void){
+
+   CU_pSuite pSuite = NULL;
+
+
+   /* initialize the CUnit test registry */
+   if ( CUE_SUCCESS != CU_initialize_registry() )
+      return CU_get_error();
+
+   /* add a suite to the registry */
+   pSuite = CU_add_suite( "Change this later lol", init_suite, clean_suite );
+   if ( NULL == pSuite ) {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* add the tests to the suite */
+   if ( (NULL == CU_add_test(pSuite, "test_add", test_add)) ||
+        (NULL == CU_add_test(pSuite, "test_minus", test_minus))
+      )
+   {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   // Run all tests using the basic interface
+   CU_basic_set_mode(CU_BRM_VERBOSE);
+   CU_basic_run_tests();
+   printf("\n");
+   CU_basic_show_failures(CU_get_failure_list());
+   printf("\n\n");
+
+   /* Clean up registry and return */
+   CU_cleanup_registry();
+   return CU_get_error();
+
+
+
 }
 
 
+void test_minus(void){
+    CU_ASSERT_EQUAL(minus(2,2),5);
+    CU_ASSERT_EQUAL(minus(2,3),5);
 
+}
 
 
 void test_add(void){
-    CU_ASSERT(add(2,2) == 5);
-    CU_ASSERT(add(2,3) == 5);
+    CU_ASSERT_EQUAL(add(2,2),5);
+    CU_ASSERT_EQUAL(add(2,3),5);
 
 }
